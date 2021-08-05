2ee806077035da077f52f5040895dd95288b0523
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-08-05 15:35:14 -0400

    clean up and time cache creation/population too

--- utils/benchmarking/handler_with_cache.rb
+++ utils/benchmarking/handler_with_cache.rb
@@ -4,12 +4,12 @@ require 'bundler/setup'
 require 'time_up'
 require 'pry'
 
+
 require 'collectionspace/mapper'
-require 'facets/array/before'
 require_relative '../../spec/helpers'
 
 config = {delimiter: ';'}
-mapper_path = File.join('..', '..', 'spec', 'fixtures', 'benchmarking', 'core_collectionobject', 'core_6-1-0_collectionobject.json')
+mapper_path = File.join(__dir__, 'fixtures', 'core_collectionobject', 'core_6-1-0_collectionobject.json')
 rec_mapper = Helpers.get_json_record_mapper(mapper_path)
 client = Helpers.core_client
 
@@ -19,10 +19,14 @@ cache_config = {
   search_identifiers: false
 }
 
+TimeUp.start(:new_cache)
 cache = CollectionSpace::RefCache.new(config: cache_config, client: client)
-popcache = CollectionSpace::RefCache.new(config: cache_config, client: client)
-Helpers.populate_core(popcache)
+TimeUp.stop(:new_cache)
 
+TimeUp.start(:new_popcache)
+popcache = Helpers.core_cache
+Helpers.populate_core(popcache)
+TimeUp.stop(:new_popcache)
 
 def handler_reusing_cache(mapper, cache, client, config)
   TimeUp.start(:hdlr_chc_reuse) do
@@ -49,7 +53,7 @@ def handler(mapper, cache, client, config)
   CS::Mapper::DataHandler.new(record_mapper: mapper, cache: cache, client: client, config: config)
 end
 
-50.times do
+10.times do
   handler_reusing_popcache(rec_mapper, popcache, client, config)
   handler_new_cache(rec_mapper, client, config)
   handler_reusing_cache(rec_mapper, cache, client, config)
