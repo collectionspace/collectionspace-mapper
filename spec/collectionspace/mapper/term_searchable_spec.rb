bdc16e69f6d6747945eb97fdd708fe19b630b4fb
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-09-03 10:17:47 -0400

    new tests for term_searchable and record_status_service methods

---
+++ spec/collectionspace/mapper/term_searchable_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+class TermClass
+  attr_reader :cache, :client
+  attr_accessor :type, :subtype, :errors
+  include CS::Mapper::TermSearchable
+
+  def initialize(cache, client)
+    @cache = cache
+    @client = client
+    @type = 'conceptauthorities'
+    @subtype = 'concept'
+    @errors = []
+  end
+  
+end
+
+RSpec.describe CollectionSpace::Mapper::TermSearchable do
+  before(:context) do
+    @cache = core_cache
+    populate_core(@cache)
+  end
+  
+  let(:term){ TermClass.new(@cache, core_client) }
+
+  describe '#in_cache?' do
+    let(:result){ term.in_cache?(val) }
+    context 'when not in cache' do
+      let(:val){ 'Tiresias' }
+      it 'returns false' do
+        expect(result).to be false
+      end
+    end
+
+    context 'when in cache' do
+      let(:val){ 'Test' }
+      it 'returns true' do
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#cached_term' do
+    let(:result){ term.cached_term(val) }
+    context 'when not in cache' do
+      let(:val){ 'Tiresias' }
+      it 'returns nil' do
+        expect(result).to be_nil
+      end
+    end
+
+    context 'when in cache' do
+      let(:val){ 'Test' }
+      it 'returns refname urn' do
+        expected = "urn:cspace:core.collectionspace.org:conceptauthorities:name(concept):item:name(Test1599650854716)'Test'"
+        expect(result).to eq(expected)
+      end
+    end
+  end
+
+  # also covers extract_refname_csid and lookup_obj_csid
+  describe '#obj_csid' do
+    let(:result){ term.obj_csid(objnum, type) }
+    let(:type){ 'collectionobjects' }
+    context 'when in cache' do
+      let(:objnum){ 'Hierarchy Test 001' }
+      
+      it 'returns csid' do
+        expect(result).to eq('16161bff-b01a-4b55-95aa')
+      end
+    end
+
+    context 'when not in cache' do
+      let(:objnum){ 'QA TEST 001' }
+      it 'returns csid' do
+        expect(result).to eq('56c04f5f-32b9-4f1d-8a4b')
+      end
+    end
+  end
+
+  describe '#term_csid' do
+    let(:result){ term.term_csid(val) }
+    context 'when in cache' do
+      let(:val){ 'Sample Concept 1' }
+      
+      it 'returns csid' do
+        expect(result).to eq('c119ed04-d99d-4ea2-bbda')
+      end
+    end
+
+    context 'when not in cache' do
+      let(:val){ 'QA TEST Concept 1' }
+      it 'returns csid' do
+        expect(result).to eq('06659851-bbed-47ab-910d')
+      end
+    end
+  end
+end
