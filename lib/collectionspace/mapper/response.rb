16de69748dbcd355bc0376f14c7d69a07333be51
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-03-29 13:15:12 -0400

    add public `add_warning` method rather than directly adding to instance variable

--- lib/collectionspace/mapper/response.rb
+++ lib/collectionspace/mapper/response.rb
@@ -20,6 +20,10 @@ module CollectionSpace
         @identifier = ''
       end
 
+      def add_warning(warning)
+        warnings << warning
+      end
+
       def merge_status_data(status_data)
         @record_status = status_data[:status]
         @csid = status_data[:csid]
