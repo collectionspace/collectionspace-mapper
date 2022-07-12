4d24d8a6ec9a77a74c78e33abb51f63d62486da1
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-12 15:13:11 -0400

    use deterministic random test order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,10 @@ require_relative './helpers'
 RSpec.configure do |config|
   config.include Helpers
 
+  # random but deterministic test order
+  config.order = :random
+  Kernel.srand config.seed
+  
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
