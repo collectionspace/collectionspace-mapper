5a416b97bf4ebcf76057d51d0b1a5b39f5240584
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-10-17 20:34:40 -0400

    move Mapper.merge_default_values tests

--- spec/collectionspace/mapper_spec.rb
+++ spec/collectionspace/mapper_spec.rb
@@ -34,4 +34,84 @@ RSpec.describe CollectionSpace::Mapper do
       end
     end
   end
+
+  describe '#merge_default_values' do
+    let(:datahash) do
+      {
+        'objectNumber' => '123',
+        'collection' => 'Permanent Collection'
+      }
+    end
+    let(:response) do
+      CollectionSpace::Mapper::Response.new(datahash)
+    end
+    let(:config){ CollectionSpace::Mapper::Config.new }
+    let(:result) do
+      CollectionSpace::Mapper.merge_default_values(
+        response,
+        config
+      )
+    end
+
+
+    context 'when no default_values specified in config' do
+      it 'does not fall over' do
+        res = result.merged_data['collection']
+        ex = 'Permanent Collection'
+        expect(res).to eq(ex)
+      end
+    end
+
+    context 'when default_values for a field is specified in config' do
+      let(:config) do
+        CollectionSpace::Mapper::Config.new(config: {
+          delimiter: ';',
+          default_values: {
+            'publishTo' => 'DPLA;Omeka'
+          }
+        })
+      end
+      context 'and no value is given for that field in the incoming data' do
+        it 'maps the default values' do
+          res = result.merged_data['publishto']
+          ex = 'DPLA;Omeka'
+          expect(res).to eq(ex)
+        end
+      end
+      context 'and value is given for that field in the incoming data' do
+        let(:datahash) do
+          {
+            'objectNumber' => '20CS.001.0001',
+            'publishto' => 'foo'
+          }
+        end
+
+        context 'and :force_defaults = false' do
+          it 'maps the value in the incoming data' do
+            res = result.merged_data['publishto']
+            ex = 'foo'
+            expect(res).to eq(ex)
+          end
+        end
+
+        context 'and :force_defaults = true' do
+          let(:config) do
+            CollectionSpace::Mapper::Config.new(config: {
+              delimiter: ';',
+              default_values: {
+                'publishTo' => 'DPLA;Omeka'
+              },
+              force_defaults: true
+            })
+          end
+
+          it 'maps the default value, overwriting value in the incoming data', services_call: true do
+            res = result.merged_data['publishto']
+            ex = 'DPLA;Omeka'
+            expect(res).to eq(ex)
+          end
+        end
+      end
+    end
+  end
 end
