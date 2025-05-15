765dc4074176abeee3fff5c944e24a96f182e059
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-05-14 21:20:35 -0400

    Refactor VocabularyTerms::Handler methods
    Make it possible to call #term_payload without doing the add or update
    in the same call.

--- lib/collectionspace/mapper/vocabulary_terms/handler.rb
+++ lib/collectionspace/mapper/vocabulary_terms/handler.rb
@@ -10,7 +10,8 @@ module CollectionSpace
       #   multiple vocabularies
       class Handler
         include Dry::Monads[:result]
-        include Dry::Monads::Do.for(:add_term)
+        include Dry::Monads::Do.for(:add_term, :added_term_params,
+                                    :term_payload)
 
         def initialize(client:)
           @client = client
@@ -18,25 +19,36 @@ module CollectionSpace
           @vocabs = CollectionSpace::Mapper::Vocabularies.new(client)
         end
 
+
         # @param vocab [String] the display name of the target Vocabulary
         # @param term [String] the term to create in the Vocabulary
+        # @param mode [:add, :update]
+        # @param opt_fields [nil, Hash{String => String}] key/value pairs for
+        #   description, source, sourcePage, and termStatus fields
+        def term_payload(vocab:, term:, mode: :add, opt_fields: nil)
+          params = case mode
+                   when :add
+                     yield added_term_params(
+                       vocab: vocab, term: term, opt_fields: opt_fields
+                     )
+                   end
+          payload = yield PayloadBuilder.call(**params)
+
+          Success(payload)
+        end
+
+        # @param vocab [String] the display name of the target Vocabulary
+        # @param term [String] the term to create in the Vocabulary
+        # @param opt_fields [nil, Hash{String => String}] key/value pairs for
+        #   description, source, sourcePage, and termStatus fields
         def add_term(vocab:, term:, opt_fields: nil)
           vocabulary = yield vocabs.by_name(vocab)
-          tid = yield get_termid(term)
-          vname = vocabulary["shortIdentifier"]
-
-          params = {
-            domain: domain,
-            csid: vocabulary["csid"],
-            name: vname,
-            term: term,
-            termid: tid
-          }
-          params[:opt_fields] = opt_fields if opt_fields
-
-          payload = yield PayloadBuilder.call(**params)
+          payload = yield term_payload(
+            vocab: vocab, term: term, mode: :add, opt_fields: opt_fields
+          )
           path = "#{vocabulary["uri"]}/items"
-          posting = yield post_term(path, payload, vname, term)
+          posting = yield post_term(
+            path, payload, vocabulary["shortIdentifier"], term)
 
           Success(posting.sub(/^.*cspace-services/, ""))
         end
@@ -44,6 +56,26 @@ module CollectionSpace
         private
 
         attr_reader :client, :domain, :vocabs
+
+        # @param vocab [String] the display name of the target Vocabulary
+        # @param term [String] the term to create in the Vocabulary
+        # @param opt_fields [nil, Hash{String => String}] key/value pairs for
+        #   description, source, sourcePage, and termStatus fields
+        def added_term_params(vocab:, term:, opt_fields: nil)
+          vocabulary = yield vocabs.by_name(vocab)
+          tid = yield get_termid(term)
+
+          params = {
+            domain: domain,
+            csid: vocabulary["csid"],
+            name: vocabulary["shortIdentifier"],
+            term: term,
+            termid: tid
+          }
+          params[:opt_fields] = opt_fields if opt_fields
+
+          Success(params)
+        end
 
         def get_termid(term)
           result = CollectionSpace::Mapper::Identifiers::ShortIdentifier.call(
