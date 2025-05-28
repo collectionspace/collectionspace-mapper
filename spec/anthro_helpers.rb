de7dff842e694f76a6d82a70023fc78bb52739e7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-05-28 18:41:45 -0400

    test: Add combined cache helper for non-core profiles

--- spec/anthro_helpers.rb
+++ spec/anthro_helpers.rb
@@ -30,6 +30,14 @@ module Helpers
   end
   memo_wise(:anthro_csid_cache)
 
+  def anthro_combined_cache
+    cache_config = base_cache_config.merge({domain: anthro_domain})
+    cache = CollectionSpace::Refcache.new(config: cache_config)
+    populate(cache, cacheable_refnames(anthro_domain), "refname")
+    populate(cache, cacheable_csids, "csid")
+  end
+  memo_wise(:anthro_combined_cache)
+
   def anthro_co_1
     get_datahash(
       path: "spec/support/datahashes/anthro/collectionobject1.json"
