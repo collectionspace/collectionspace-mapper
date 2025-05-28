dda12ea67b2fa5cd3acbf727865dfae9e4da2316
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-05-28 18:48:04 -0400

    test: Verify this works with allinone caching

--- spec/collectionspace/mapper/dates/structured_date_handler_spec.rb
+++ spec/collectionspace/mapper/dates/structured_date_handler_spec.rb
@@ -36,5 +36,36 @@ RSpec.describe CollectionSpace::Mapper::Dates::StructuredDateHandler do
         expect(result).to eq(refname)
       end
     end
+
+    context "with single record type handler" do
+      let(:handler) do
+        setup_single_record_type_handler(
+          profile: "anthro",
+          mapper: "https://raw.githubusercontent.com/collectionspace/"\
+            "cspace-config-untangler/refs/heads/main/data/mappers/"\
+            "community_profiles/release_8_1_1/anthro/"\
+            "anthro_9-0-0_collectionobject.json"
+        )
+      end
+
+      context "when term is cached" do
+        let(:domain) { "c.anthro.collectionspace.org" }
+        it "returns expected refname" do
+          handler.cache
+            .put_vocab_term("dateera", "CE", refname, "refname")
+          expect(result).to eq(refname)
+        end
+      end
+
+      context "when term is not cached" do
+        let(:domain) { "anthro.collectionspace.org" }
+        it "returns expected refname" do
+          %w[refname csid].each do |type|
+            handler.cache.remove_vocab_term("dateera", "CE", type)
+          end
+          expect(result).to eq(refname)
+        end
+      end
+    end
   end
 end
