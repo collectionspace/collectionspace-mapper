a8d7bb0b43d63607c974969401be11dbc2f8e6be
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-25 18:47:35 -0400

    feat: Implement updating vocab terms
    This includes added supporting field values to terms, and changing the
    displayName value

--- lib/collectionspace/mapper/vocabulary_terms/handler.rb
+++ lib/collectionspace/mapper/vocabulary_terms/handler.rb
@@ -11,7 +11,7 @@ module CollectionSpace
       class Handler
         include Dry::Configurable
         include Dry::Monads[:result]
-        include Dry::Monads::Do.for(:add_term, :added_term_params,
+        include Dry::Monads::Do.for(:add_term, :update_term, :added_term_params,
           :term_payload)
 
         attr_reader :client
@@ -32,7 +32,8 @@ module CollectionSpace
         end
 
         # @param vocab [String] the display name of the target Vocabulary
-        # @param term [String] the term to create in the Vocabulary
+        # @param term [String, Hash] if mode is :add, the term string to create;
+        #   if mode is :update, existing record for term, returned by searcher
         # @param mode [:add, :update]
         # @param opt_fields [nil, Hash{String => String}] key/value pairs for
         #   description, source, sourcePage, and termStatus fields
@@ -41,6 +42,10 @@ module CollectionSpace
           when :add
             yield added_term_params(
               vocab: vocab, term: term, opt_fields: opt_fields
+            )
+          when :update
+            yield updated_term_params(
+              vocab: vocab, term_data: term, opt_fields: opt_fields
             )
           end
           payload = yield PayloadBuilder.call(**params)
@@ -55,7 +60,7 @@ module CollectionSpace
         def add_term(vocab:, term:, opt_fields: nil)
           vocabulary = yield vocabs.by_name(vocab)
           payload = yield term_payload(
-            vocab: vocab, term: term, mode: :add, opt_fields: opt_fields
+            vocab: vocabulary, term: term, mode: :add, opt_fields: opt_fields
           )
           path = "#{vocabulary["uri"]}/items"
           posting = yield post_term(
@@ -71,15 +76,23 @@ module CollectionSpace
         #   description, source, sourcePage, and termStatus fields
         def update_term(vocab:, term:, opt_fields: nil)
           vocabulary = yield vocabs.by_name(vocab)
-          payload = yield term_payload(
-            vocab: vocab, term: term, mode: :update, opt_fields: opt_fields
+          termresp = searcher.call(
+            value: term, type: "vocabularies",
+            subtype: vocabulary["shortIdentifier"]
           )
-          path = "#{vocabulary["uri"]}/items"
-          posting = yield post_term(
-            path, payload, vocabulary["shortIdentifier"], term
-          )
+          unless termresp
+            return Failure("The term \"#{term}\" does not exist in #{vocab}")
+          end
 
-          Success(posting.sub(/^.*cspace-services/, ""))
+          termrec = termresp.dig("list_item", 0)
+          payload = yield term_payload(
+            vocab: vocabulary, term: termrec, mode: :update,
+            opt_fields: opt_fields
+          )
+          path = termrec["uri"]
+          putting = yield put_term(path, payload)
+
+          Success(putting)
         end
 
         private
@@ -91,18 +104,42 @@ module CollectionSpace
         # @param opt_fields [nil, Hash{String => String}] key/value pairs for
         #   description, source, sourcePage, and termStatus fields
         def added_term_params(vocab:, term:, opt_fields: nil)
-          vocabulary = yield vocabs.by_name(vocab)
           tid = yield get_termid(term)
 
           params = {
             domain: domain,
-            csid: vocabulary["csid"],
-            name: vocabulary["shortIdentifier"],
+            csid: vocab["csid"],
+            name: vocab["shortIdentifier"],
             term: term,
             term_data: {"shortIdentifier" => tid}
           }
           params[:opt_fields] = opt_fields if opt_fields
 
+          Success(params)
+        end
+
+        # @param vocab [String] the display name of the target Vocabulary
+        # @param term_data [Hash] existing record for the term
+        # @param opt_fields [nil, Hash{String => String}] key/value pairs for
+        #   description, source, sourcePage, and termStatus fields
+        def updated_term_params(vocab:, term_data:, opt_fields: nil)
+          params = {
+            domain: domain,
+            csid: vocab["csid"],
+            name: vocab["shortIdentifier"],
+            term: term_data["displayName"],
+            term_data: {"shortIdentifier" => term_data["shortIdentifier"]}
+          }
+          return Success(params) unless opt_fields
+
+          new_term = opt_fields.fetch("displayName", nil)
+          unless new_term
+            params[:opt_fields] = opt_fields
+            return Success(params)
+          end
+
+          params[:term] = opt_fields.delete("displayName")
+          params[:opt_fields] = opt_fields
           Success(params)
         end
 
@@ -126,6 +163,21 @@ module CollectionSpace
             Failure("#{vname}/#{term} already exists")
           when 201
             Success(result.result.headers["location"])
+          else
+            Failure(result)
+          end
+        end
+
+        def put_term(path, payload)
+          result = client.put(path, payload)
+        rescue => err
+          Failure(err)
+        else
+          case result.status_code
+          when 200
+            Success(result.result.dig(
+              "document", "collectionspace_core", "uri"
+            ))
           else
             Failure(result)
           end
