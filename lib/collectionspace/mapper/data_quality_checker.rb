2da4d1d3d2714b2d0352c53f01eba328986536c7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-12-10 20:51:49 -0500

    feat: Validate boolean, float, and integer data types

--- lib/collectionspace/mapper/data_quality_checker.rb
+++ lib/collectionspace/mapper/data_quality_checker.rb
@@ -3,10 +3,14 @@
 module CollectionSpace
   module Mapper
     class DataQualityChecker
+      # Data types that we need to validate because CollectionSpace doesn't
+      TO_VALIDATE = %w[boolean integer float]
+
       class << self
         def checkable?(mapping)
           true if mapping.source_type == "optionlist" ||
-            mapping.datacolumn["refname"]
+            mapping.datacolumn["refname"] ||
+            TO_VALIDATE.include?(mapping.data_type)
         end
       end
 
@@ -20,19 +24,48 @@ module CollectionSpace
         @column = mapping.datacolumn
         @field = mapping.fieldname
 
-        case mapping.source_type
-        when "authority"
+        if %w[authority vocabulary].include?(mapping.source_type) &&
+            column.match?("refname")
           validate_all_refnames if @column["refname"]
-        when "vocabulary"
-          validate_all_refnames if @column["refname"]
-        when "optionlist"
+        elsif mapping.source_type == "optionlist"
           check_all_opt_list_vals
+        elsif TO_VALIDATE.include?(mapping.data_type)
+          validate_data_type
         end
       end
 
       private
 
       attr_reader :response, :column
+
+      def validate_data_type
+        vals = data.first.is_a?(String) ? data : data.flatten
+        result = vals.reject do |v|
+          v.blank? ||
+            v == "%NULLVALUE%" ||
+            v == CollectionSpace::Mapper.bomb ||
+            valid_data_type?(v)
+        end
+        add_data_type_error(result) unless result.empty?
+      end
+
+      def valid_data_type?(v) = send(:"valid_#{mapping.data_type}?", v)
+
+      def valid_boolean?(v) = v.match?(/^(?:true|false)$/)
+
+      def valid_float?(v) = v.match?(/^\d+(?:\.\d+)?$/)
+
+      def valid_integer?(v) = v.match?(/^\d+$/)
+
+      # @param [Array<String>]
+      def add_data_type_error(vals)
+        response.add_error({
+          category: :invalid_value_for_data_type,
+          message: "The #{column} column's data type is "\
+            "#{mapping.data_type}. The following values are not valid "\
+            "#{mapping.data_type}s: #{vals.join("|")}"
+        })
+      end
 
       def validate_all_refnames
         if data.first.is_a?(String)
