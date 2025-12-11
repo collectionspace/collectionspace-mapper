332508b2f0d3acc7363e39ce05c986765ae80f1f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-12-10 20:51:49 -0500

    feat: Validate boolean, float, and integer data types

--- spec/collectionspace/mapper/data_quality_checker_spec.rb
+++ spec/collectionspace/mapper/data_quality_checker_spec.rb
@@ -16,6 +16,117 @@ RSpec.describe CollectionSpace::Mapper::DataQualityChecker do
   end
   let(:response) { double("Response") }
 
+  context "when data_type = integer" do
+    let(:maphash) do
+      {
+        fieldname: "age",
+        datacolumn: "age",
+        transforms: {},
+        data_type: "integer",
+        source_type: "na",
+        opt_list_values: []
+      }
+    end
+
+    context "with invalid value" do
+      let(:data) { ["1,234", "42", "about 42", "%NULLVALUE%"] }
+
+      it "adds expected error" do
+        err = {
+          category: :invalid_value_for_data_type,
+          message: "The age column's data type is "\
+            "integer. The following values are not valid "\
+            "integers: 1,234|about 42"
+        }
+        expect(response).to receive(:add_error).with(err).once
+        checker
+      end
+    end
+
+    context "with placeholder, empty, and valid values" do
+      let(:data) { ["%NULLVALUE%", "", "42"] }
+
+      it "does not add error" do
+        expect(response).not_to receive(:add_error)
+        checker
+      end
+    end
+  end
+
+  context "when data_type = float" do
+    let(:maphash) do
+      {
+        fieldname: "anthroownershippriceamount",
+        datacolumn: "anthroownershippriceamount",
+        transforms: {},
+        data_type: "float",
+        source_type: "na",
+        opt_list_values: []
+      }
+    end
+
+    context "with invalid value" do
+      let(:data) { [["33", "$100.00"], ["1.2", "1,234"]] }
+
+      it "adds expected error" do
+        err = {
+          category: :invalid_value_for_data_type,
+          message: "The anthroownershippriceamount column's data type is "\
+            "float. The following values are not valid "\
+            "floats: $100.00|1,234"
+        }
+        expect(response).to receive(:add_error).with(err).once
+        checker
+      end
+    end
+
+    context "with placeholder, empty, and valid values" do
+      let(:data) { ["%NULLVALUE%", "", "1.2"] }
+
+      it "does not add error" do
+        expect(response).not_to receive(:add_error)
+        checker
+      end
+    end
+  end
+
+  context "when data_type = boolean" do
+    let(:maphash) do
+      {
+        fieldname: "nagprareportfiled",
+        datacolumn: "nagprareportfiled",
+        transforms: {},
+        data_type: "boolean",
+        source_type: "na",
+        opt_list_values: []
+      }
+    end
+
+    context "with invalid value" do
+      let(:data) { [["maybe", "true"], ["n", "on god"]] }
+
+      it "adds expected error" do
+        err = {
+          category: :invalid_value_for_data_type,
+          message: "The nagprareportfiled column's data type is "\
+            "boolean. The following values are not valid "\
+            "booleans: maybe|n|on god"
+        }
+        expect(response).to receive(:add_error).with(err).once
+        checker
+      end
+    end
+
+    context "with placeholder, empty, and valid values" do
+      let(:data) { ["%NULLVALUE%", "", "false"] }
+
+      it "does not add error" do
+        expect(response).not_to receive(:add_error)
+        checker
+      end
+    end
+  end
+
   context "when source_type = optionlist" do
     let(:maphash) do
       {
@@ -42,7 +153,7 @@ RSpec.describe CollectionSpace::Mapper::DataQualityChecker do
     end
 
     context "with placeholder, empty, and valid values" do
-      let(:data) { ["%NULLVALUE%", "", "permanent-collection"] }
+      let(:data) { [["%NULLVALUE%", ""], ["permanent-collection"]] }
 
       it "does not add warning" do
         expect(response).not_to receive(:add_warning)
